0e66083a4f2aea55638764e4f63e278697d7c89e
Coney Wu <kunwu@thoughtworks.com>
2014-01-10 15:48:58 +0800

    add a simple log that prints filename and line no

---
+++ js_log.h
@@ -0,0 +1,20 @@
+/**
+  * @file   js_log.h
+  * @brief  A simple log implementation
+  * @author ConeyWu
+  * @date   10/01/2014
+  */
+#ifndef __JS_LOG_H__
+#define __JS_LOG_H__
+
+
+#define __JS_TOSTRING(x)        #x
+#define _JS_TOSTRING(x)         __JS_TOSTRING(x)
+
+#define writelog(level, fmt...)        \
+    printk(level __FILE__ ":" _JS_TOSTRING(__LINE__)" - " fmt)
+
+#define logdebug(fmt...)        writelog(KERN_DEBUG, fmt)
+#define loginfo(fmt...)        writelog(KERN_INFO, fmt)
+
+#endif // __JS_LOG_H__
