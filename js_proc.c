c560e85c8b74ef43885c0ceeeebe8ada3881ab5a
Coney Wu <coney.wu@gmail.com>
2014-04-29 23:52:08 +0800

    add proc dir for joystick driver

--- js_proc.c
+++ js_proc.c
@@ -10,17 +10,17 @@
 
 static char js_proc_buffer[JS_PROC_BUFSIZE];
 
-static int js_proc_show(struct seq_file *m, void *v) {
-    logdebug("proc read (/proc/%s) called\n", JS_PROC_NAME);
+static int js_proc_test_show(struct seq_file *m, void *v) {
+    logdebug("proc read (/proc/%s) called\n", JS_PROC_TEST);
     seq_printf(m, "Hello Kitty!\n");
     return 0;
 }
 
-static int js_proc_open(struct inode *inode, struct file *file) {
-    return single_open(file, js_proc_show, NULL);
+static int js_proc_test_open(struct inode *inode, struct file *file) {
+    return single_open(file, js_proc_test_show, NULL);
 }
 
-static ssize_t js_proc_write(struct file *file, const char *buffer, size_t count, loff_t *offset) {
+static ssize_t js_proc_test_write(struct file *file, const char *buffer, size_t count, loff_t *offset) {
     int button = 0;
     logdebug("write %zu bytes, offset %p\n", count, offset);
     if (count >= JS_PROC_BUFSIZE) {
@@ -42,23 +42,34 @@ static ssize_t js_proc_write(struct file *file, const char *buffer, size_t count
     return count;
 }
 
-static const struct file_operations js_proc_fops = {
+static const struct file_operations js_proc_config_fops = {
         .owner = THIS_MODULE,
-        .open = js_proc_open,
+        .open = js_proc_test_open,
         .read = seq_read,
-        .write  = js_proc_write,
+        .write  = js_proc_test_write,
         .llseek = seq_lseek,
         .release = single_release,
 };
 
+
+
+static struct proc_dir_entry *js_proc_root = NULL;
+
 int js_proc_init(void) {
-    if (!proc_create(JS_PROC_NAME, S_IRUGO | S_IWUGO, NULL, &js_proc_fops)) {
-        return -ENOMEM;
+
+    if ((js_proc_root = proc_mkdir_mode(JS_PROC_NAME, S_IRUGO | S_IWUGO, NULL)) == NULL
+        || !proc_create(JS_PROC_TEST, S_IRUGO | S_IWUGO, js_proc_root, &js_proc_config_fops)) {
+            js_proc_clear();
+            return -ENOMEM;
     }
 
     return 0;
 }
 
 void js_proc_clear(void) {
-    remove_proc_entry(JS_PROC_NAME, NULL);
+    if (js_proc_root) {
+        remove_proc_entry(JS_PROC_TEST, js_proc_root);
+        remove_proc_entry(JS_PROC_NAME, NULL);
+        js_proc_root = 0;
+    }
 }
--- js_proc.h
+++ js_proc.h
@@ -7,7 +7,9 @@
 #ifndef __JS_PROC_H__
 #define __JS_PROC_H__
 
-#define JS_PROC_NAME            "twjs"
+#define JS_PROC_NAME            "twjoystick"
+#define JS_PROC_CONFIG          "config"
+#define JS_PROC_TEST            "test"
 #define JS_PROC_BUFSIZE         2048
 
 int js_proc_init(void);
