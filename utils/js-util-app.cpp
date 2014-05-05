4874988a6c0648a59b985d2969248f1b3b57460e
Coney Wu <coney.wu@gmail.com>
2014-05-06 00:07:52 +0800

    remove js utils

--- utils/js-util-app.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "js-util-app.h"
-
-int JoystickUtilApp::main(const std::vector < std::string > & args) {
-    printf("hehe\n");
-    return 0;
-}
--- utils/js-util.cpp
+++
@@ -1,7 +0,0 @@
-#include "js-util-app.h"
-
-int main(int argc, char ** argv) {
-    JoystickUtilApp app;
-    app.init(argc, argv);
-    app.run();
-}
